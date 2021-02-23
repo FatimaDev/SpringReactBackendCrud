19412371ddc0ea2d0bc18af62d175eb727b5ff88
FatimaDev <yaya.fatima.ezzahra@gmail.com>
2021-02-23 21:48:10 +0100

    spring boot

---
+++ springReact-backend-crud/src/main/java/com/yaya/fatima/SpringReactBackendCrudApplication.java
@@ -0,0 +1,13 @@
+package com.yaya.fatima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactBackendCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactBackendCrudApplication.class, args);
+	}
+
+}
---
+++ springReact-backend-crud/src/test/java/com/yaya/fatima/SpringReactBackendCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaya.fatima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactBackendCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
